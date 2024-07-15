1f820adfba61af2ebcddc6505631c208b419fd97
Leejungchan1 <wjdcks0123@naver.com>
2024-07-15 17:47:29 +0900

    hollys1

---
+++ js/hollys.js
@@ -0,0 +1,47 @@
+$(function(){
+    //메뉴
+    $(".main > li, .sub_bg").hover(function(){
+        $(".sub, .sub_bg").stop().slideDown()
+    },function(){
+        $(".sub, .sub_bg").stop().slideUp()
+    }) //hover
+
+    //slick
+
+    $(".move").slick({
+        autoplay:true,
+        fade:true,
+        dots:true,
+        arrows:false,
+        autoplaySpeed:2000,
+        adaptiveHeight:true
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})//jQuery
